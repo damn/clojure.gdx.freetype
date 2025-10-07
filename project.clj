5a719b264d68c114285ef24a27080e29c78bd8fc
Michael Sappler <michaelsappler@gmail.com>
2025-10-07 12:45:23 +0530

    Remove codox

--- project.clj
+++ project.clj
@@ -1,6 +1,4 @@
 (defproject clojure.gdx.freetype "1.13.5"
   :dependencies [[org.clojure/clojure "1.12.0"]
                  [com.badlogicgames.gdx/gdx-freetype          "1.13.5"]
-                 [com.badlogicgames.gdx/gdx-freetype-platform "1.13.5" :classifier "natives-desktop"]]
-  :codox {:source-uri "https://github.com/damn/gdl.graphics.freetype/blob/main/{filepath}#L{line}"
-          :metadata {:doc/format :markdown}})
+                 [com.badlogicgames.gdx/gdx-freetype-platform "1.13.5" :classifier "natives-desktop"]])
